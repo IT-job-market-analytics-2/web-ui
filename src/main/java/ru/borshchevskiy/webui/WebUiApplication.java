8228c91f78ba102725e7f5e04f17e6c742cca88e
Ilya <freer1der.msf@gmail.com>
2024-05-16 18:40:01 +0300

    Created project w/ Spring Boot 3, Web and Thymeleaf starters

---
+++ src/main/java/ru/borshchevskiy/webui/WebUiApplication.java
@@ -0,0 +1,13 @@
+package ru.borshchevskiy.webui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebUiApplication.class, args);
+    }
+
+}
