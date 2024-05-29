0f0b4e4028f5b849175763b1a6eaba0c1b6c0147
Ilya <freer1der.msf@gmail.com>
2024-05-29 23:23:26 +0300

    Updated UserDto, added exception handlers for sign-in aand sign-up

--- src/main/java/ru/borshchevskiy/webui/dto/user/UserDto.java
+++ src/main/java/ru/borshchevskiy/webui/dto/user/UserDto.java
@@ -1,4 +1,20 @@
 package ru.borshchevskiy.webui.dto.user;
 
 public class UserDto {
+    private String username;
+
+    public UserDto() {
+    }
+
+    public UserDto(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
