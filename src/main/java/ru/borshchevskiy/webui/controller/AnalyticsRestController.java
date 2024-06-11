62df99d8bb839a1aed5382c2b29015c6f510bf76
Ilya <freer1der.msf@gmail.com>
2024-06-11 15:00:50 +0300

    New Controller for analytics requests

--- src/main/java/ru/borshchevskiy/webui/controller/AnalyticsRestController.java
+++ src/main/java/ru/borshchevskiy/webui/controller/AnalyticsRestController.java
@@ -1,4 +1,40 @@
 package ru.borshchevskiy.webui.controller;
 
-public class AnalyticsController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.borshchevskiy.webui.dto.analytics.AnalyticsDto;
+import ru.borshchevskiy.webui.dto.subscription.SubscriptionDto;
+import ru.borshchevskiy.webui.service.RestApiClientService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/analytics")
+public class AnalyticsRestController {
+
+    private final RestApiClientService restApiClientService;
+
+    public AnalyticsRestController(RestApiClientService restApiClientService) {
+        this.restApiClientService = restApiClientService;
+    }
+
+    @GetMapping(value = "/queries", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public List<SubscriptionDto> getAvailableQueries () {
+        return restApiClientService.getaAvailableQueries();
+    }
+
+    @GetMapping(value = "/{query}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public List<AnalyticsDto> getAnalyticsByQuery (@PathVariable("query") String query) {
+        return restApiClientService.getAnalyticsByQuery(query);
+    }
+
+    @GetMapping(value = "/recent", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public List<AnalyticsDto> getRecentAnalytics () {
+        return restApiClientService.getRecentAnalytics();
+    }
 }
